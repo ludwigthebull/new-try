079245f4315b8fb80a3687327ccf9673e5c6ff36
ludwigthebull <lkb43@cam.ac.uk>
2016-10-08 10:14:51 +0100

    Update index.js

--- src/client/components/Header/index.js
+++ src/client/components/Header/index.js
@@ -4,4 +4,28 @@ const Header = (props) => {
   return <header>Ficken</header>
 };
 
+import React, { Component } from 'react';
+import { AppRegistry, View, Image } from 'react-native';
+
+class DisplayAnImage extends Component {
+  render() {
+    return (
+      <View>
+        <Image
+          source={require('./img/favicon.png')}
+        />
+        <Image
+          style={{width: 50, height: 50}}
+          source={{uri: 'https://facebook.github.io/react/img/logo_og.png'}}
+        />
+      </View>
+    );
+  }
+}
+
+// App registration and rendering
+AppRegistry.registerComponent('DisplayAnImage', () => DisplayAnImage);
+
 export default Header;
+
+
